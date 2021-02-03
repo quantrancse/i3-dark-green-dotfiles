1d6a47692716c35550b988076e1cfec319164f7a
quantrancse <quan.tran.itbk@hcmut.edu.vn>
2021-02-03 16:04:04 +0700

    Update polybar

--- .config/polybar/scripts/getWeather.py
+++ .config/polybar/scripts/getWeather.py
@@ -1,16 +1,37 @@
+#!/bin/python
+# -*- coding: utf-8 -*-
+
+# Procedure
+# Surf to https://openweathermap.org/city
+# Fill in your CITY
+# e.g. Antwerp Belgium
+# Check url
+# https://openweathermap.org/city/2803138
+# you will the city code at the end
+# create an account on this website
+# create an api key (free)
+# LANG included thanks to krive001 on discord
+
+
 import requests
-from bs4 import BeautifulSoup
-import html5lib
+
+CITY = "1580578"
+API_KEY = "756edce7e9d4c385ef9499a53492678c"
+UNITS = "Metric"
+UNIT_KEY = "C"
+#UNIT_KEY = "F"
+LANG = "en"
+#LANG = "nl"
+#LANG = "hu"
 
 try:
-    url = "https://www.timeanddate.com/weather/vietnam/ho-chi-minh"
-    soup = BeautifulSoup((requests.get(url)).text, 'html5lib')
-    weather_test = soup.find(class_="h2")
-    tempra = int(((weather_test).text.split("\xa0"))[0])
-    if "F" in ((weather_test).text.split("\xa0"))[1]:
-        tempra = round((tempra - 32 ) * (5/9))
-except requests.exceptions.ConnectionError:
-    tempra = ""
-
-if tempra != "":
-    print(str(tempra)+"°C")
+    REQ = requests.get("http://api.openweathermap.org/data/2.5/weather?id={}&lang={}&appid={}&units={}".format(CITY, LANG,  API_KEY, UNITS))
+    # HTTP CODE = OK
+    if REQ.status_code == 200:
+        CURRENT = REQ.json()["weather"][0]["description"].capitalize()
+        TEMP = int(float(REQ.json()["main"]["temp"]))
+        print("{}, {} °{}".format(CURRENT, TEMP, UNIT_KEY))
+    else:
+        print("Error")
+except:
+    print("Error")
